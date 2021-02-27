d7a829aaf45b0c1b5ae03c64dae692d858f1bfea
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2021-02-27 21:32:55 +0000

    xd

--- pset/pset3/music/helpers.c
+++ pset/pset3/music/helpers.c
@@ -1,28 +1,66 @@
-// Helper functions for music
-
+#include <string.h>
 #include <cs50.h>
+#include <math.h>
 
+// librerias
 #include "helpers.h"
-
-struct Notes {
-    char Do = '';
-}
 
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
-}
+    int num1 = atoi(fraction[0])
+    int num2 = atoi(fraction[2])
 
+    return num1 * 8 / num2;
+}
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    const string NOTES[] = {"C", "C#", "D", "D#", "E", "F",
+                            "F#", "G", "G#", "A", "A#", "B"
+                           };
+    char notaPrincipal[2] = {note[0], '\0' };
+    int nota_final = 0;
+    while (strcmp(NOTES[nota_final], notaPrincipal))
+    {
+        nota_final++;
+    }
+
+    // aplicando la fórmula frecuencia(nota,octava) := 440 * exp( (octava-3)+ ((nota-9)/12) *ln(2) )
+    nota_final = nota_final - 9;
+
+    if (note[1] == '#')
+    {
+        nota_final++;
+        int octavas = note[2] - '0';
+        nota_final += (octavas - 4) * 12;
+    }
+    else if (note[1] == 'b')
+    {
+        nota_final--;
+        int octavas = note[2] - '0';
+        nota_final += (octavas - 4) * 12;
+
+    }
+    else
+    {
+        int octavas = note[1] - '0';
+        nota_final += (octavas - 4) * 12;
+    }
+    return round(pow(2.0, (float) nota_final / 12) * 440);
+
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    if(s == '\0') return true
-    return false
+    int descanso = strcmp(s, "");
+    if (descanso == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
--- pset/pset3/music/notes.c
+++ pset/pset3/music/notes.c
@@ -2,6 +2,7 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "helpers.h"
